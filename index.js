aa11e7db7ce43a76539b8ae081025beaa7262975
“saschakala” <mxsaschakala@gmail.com>
2020-12-04 14:52:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+let eggs = new Breakfast("eggs", "coffee")
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert
+    }
+}
